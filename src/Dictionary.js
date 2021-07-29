1bc0036a54dc78923880efe7167ab0dffc49c9ce
Barbara Oleszek <barbaraoleszek@gmail.com>
2021-07-29 14:03:50 +0200

    added dictionary API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,19 +1,26 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
 
     let [keyword, setKeyword] = useState("");
 
+    function handleResponse(response) {
+        console.log(response.data[0]);
+    }
+
     function handleKeywordChange(event) {
         setKeyword(event.target.value);
-        console.log(keyword);
+        
     }
 
     function search(event) {
         event.preventDefault();
+        // documentation dictionaryapi.dev
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
 
-        
+        axios.get(apiUrl).then(handleResponse);    
     }
     
     return (
