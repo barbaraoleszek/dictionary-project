dea464961716c07d2933ca9635c117b12a6fdfec
Barbara Oleszek <barbaraoleszek@gmail.com>
2021-07-29 17:37:50 +0200

    added Synonyms component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms.js";
 
 export default function Meaning(props) {
 
@@ -10,6 +11,7 @@ export default function Meaning(props) {
                     <div key={index}>
                         <p>{definition.definition}</p>
                         <p>{definition.example}</p>
+                        <Synonyms synonyms={definition.synonyms}/>
                     </div>
                 )
             })}
